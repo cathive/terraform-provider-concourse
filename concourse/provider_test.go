2c027f606f5b6f0640008fa47813b66d7ef7e177
NickLarsenNZ <nick@aptiv.co.nz>
2018-10-07 23:20:53 +1300

    Default Provider test

--- concourse/provider_test.go
+++ concourse/provider_test.go
@@ -6,7 +6,6 @@ import (
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
-func testAccPreCheck(t *testing.T) {}
 var testAccProviders map[string]terraform.ResourceProvider
 var testAccProvider *schema.Provider
 
@@ -15,4 +14,19 @@ func init()  {
 	testAccProviders = map[string]terraform.ResourceProvider{
 		"concourse": testAccProvider,
 	}
+}
+
+func TestProvider(t *testing.T) {
+	if err := Provider().(*schema.Provider).InternalValidate(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+}
+
+func TestProvider_impl(t *testing.T) {
+	var _ terraform.ResourceProvider = Provider()
+}
+
+func testAccPreCheck(t *testing.T) {
+	// Should anything be checked before acceptance tests are run?
+	t.Log("No acceptance test pre-checks defined...")
 }
